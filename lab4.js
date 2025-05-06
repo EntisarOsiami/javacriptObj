4221d09c24223c3a6247bdfd56a7dbe33f673011
Entisar <Code.g.light@gmail.com>
2025-05-06 13:02:00 +0300

    ...

--- lab4.js
+++ lab4.js
@@ -1,26 +1,34 @@
 saudiAirlines = {
-    airlineName: "Saudi Airlines",
-    country: "Saudi Arabia",
-    fleetSize: 200,
-    destinations: ["Jeddah", "Riyadh", "Dammam", "Medina","London", "Paris", "New York"],
-    inOperation: true,
-    bookFlight: function (destinations){
-        if (this.destinations.includes(destinations)) {
-            console.log(`Flight to ${destinations} is available. The flight is booked.`);
-        }
-        else {
-            console.log(`Flight to ${destinations} is not available.`);
-        }
-    },
-    addDestination: function (newDestination) {
-        this.destinations.push(newDestination);
-    },
+  airlineName: "Saudi Airlines",
+  country: "Saudi Arabia",
+  fleetSize: 200,
+  destinations: [
+    "Jeddah",
+    "Riyadh",
+    "Dammam",
+    "Medina",
+    "London",
+    "Paris",
+    "New York",
+  ],
+  inOperation: true,
+  bookFlight: function (destinations) {
+    if (this.destinations.includes(destinations)) {
+      console.log(
+        `Flight to ${destinations} is available. The flight is booked.`
+      );
+    } else {
+      console.log(`Flight to ${destinations} is not available.`);
+    }
+  },
+  addDestination: function (newDestination) {
+    this.destinations.push(newDestination);
+  },
 
-    getSummary: function () {
-return `${this.airlineName} operates in ${this.country}, has a ${this.fleetSize} planes and flies to ${this.destinations}`    },
-
-}
-
+  getSummary: function () {
+    return `${this.airlineName} operates in ${this.country}, has a ${this.fleetSize} planes and flies to ${this.destinations}`;
+  },
+};
 
 saudiAirlines.bookFlight("Riyadh");
 saudiAirlines.addDestination("Abha");
